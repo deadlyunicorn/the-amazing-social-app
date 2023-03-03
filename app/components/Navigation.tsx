1fe1b0fc52ba4af22e3cfe434ab30a988f18befe
deadly-unicorn <linatic@pm.me>
2023-03-03 02:01:36 +0200

    created a navigation component

---
+++ app/components/Navigation.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link"
+
+const NavigationBar = () => (
+  <nav className="absolute w-full h-16 bottom-0
+  border-t border-zinc-400 rounded-t-md bg-black bg-opacity-25
+  flex justify-evenly items-center">
+
+    <NavItem icon="🏠"/>
+    <NavItem icon="🌊"/>      
+    <NavItem icon="💬"/>      
+    <NavItem icon="🔒"/>      
+  </nav>
+)
+
+
+const NavItem = (props:{icon:string}) => (
+  <Link
+  href="/" 
+  className="text-4xl h-12
+  flex items-center justify-center
+  border-l-2 border-zinc-400 hover:border-zinc-200 bg-neutral-900 hover:bg-neutral-800 rounded-lg p-2">
+    {props.icon}
+  </Link>
+)
+
+export default NavigationBar
