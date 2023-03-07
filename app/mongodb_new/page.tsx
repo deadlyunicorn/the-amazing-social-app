9aa1eae58d5d70e0e798c2a754cb1149d1e88a58
deadly-unicorn <linatic@pm.me>
2023-03-08 01:14:53 +0200

    Testing route handlers. Nothing spectacular yet.

---
+++ app/mongodb_new/page.tsx
@@ -0,0 +1,16 @@
+
+const MongoFunction = async() => {
+
+  const query = "Titanic"
+  //const response = await fetch(`/api/mongodb_new/route?query=${encodeURIComponent(query)}`)
+
+  return(
+    <>
+      hello world!!
+      <br/>
+      This is a whole new worldwhole new worldwhole new worldwhole new worldwhole new worldwhole new world
+    </>
+  );
+}
+
+export default MongoFunction;
