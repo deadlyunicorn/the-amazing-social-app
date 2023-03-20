c6632cf79debf8d2afc7f8be9e2d3ff0e3ed38f9
deadly-unicorn <linatic@pm.me>
2023-03-21 01:54:03 +0200

    update file location

---
+++ app/login/page.tsx
@@ -0,0 +1,75 @@
+'use client'
+
+import "@/app/components/Styles/styles.css"
+
+import { FormEvent, useEffect, useState } from "react";
+
+import {app} from "@/app/components/appObject"
+import * as Realm from "realm-web"
+
+import UserDetails from "@/app/components/userDetails";
+import LogoutButton from "@/app/components/Login_Logout_Register/logoutButton";
+
+import LoginForm from "../components/Login_Logout_Register/email/login_form";
+import ErrorHandler from "@/app/components/Login_Logout_Register/email/error_handling";
+
+
+const UserPage = () =>{
+
+  const [pageLoad,setPageLoad]=useState(false);
+  const [user,setUser]=useState<Realm.User | null>(null);
+  const [errorCode,setErrorCode]=useState<string|null>(null);
+
+
+  const [email,setEmail]=useState<string>("");
+  const [password,setPassword]=useState<string>("");
+
+  useEffect(()=>{
+    setUser(app.currentUser);
+    setTimeout(()=>{
+      setPageLoad(true);
+    },500);
+  },[user])
+
+  useEffect(()=>{ //I could either use useRef to keep a static
+    //version of email for each form submit
+    //this will close the prompt => unable the user
+    //to send confirmation to the wrong email.
+    setErrorCode(null);
+  },[email])
+
+  return(
+    <div>
+
+      <div className="bg-white p-4 rounded-lg w-96 h-44 mb-4 duration-700 ">
+        <div
+          data-pageload={pageLoad}
+          className="data-[pageload=true]:inline animate-hidden hidden">
+
+            {user?
+            <>
+              <UserDetails user={user}/>
+              <LogoutButton setUser={setUser}/>
+              <span>&nbsp;to login to another account.</span>
+            </>:
+            <>
+              Login today!
+              <LoginForm setErrorCode={setErrorCode} email={email} setEmail={setEmail} password={password} setPassword={setPassword}/>
+
+            </>
+            }
+
+
+        </div>
+      </div>
+
+      {!user&&errorCode&&<ErrorHandler errorCode={errorCode} setErrorCode={setErrorCode} email={email}/>}
+    </div>
+  )
+}
+
+
+
+
+
+export default UserPage;
