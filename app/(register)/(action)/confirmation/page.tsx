bc2825109a79bdacccc4c83109492728076a510e
deadly-unicorn <linatic@pm.me>
2023-03-21 15:17:28 +0200

    search params and interface for confirmation

---
+++ app/(register)/(action)/confirmation/page.tsx
@@ -0,0 +1,70 @@
+import {app} from "@/app/components/appObject"
+
+
+interface errorInterface{
+  errorCode:string
+}
+
+export default async function DynamicPage(
+  
+  //{params}=params.params
+  {searchParams:{
+    token,
+    tokenId}}:
+  {searchParams:{
+    token:string|null,
+    tokenId:string|null}}
+){
+
+
+  
+  try{
+    await app.emailPasswordAuth.confirmUser({})
+    //if error the following will be skipped
+    return(<ConfirmationSuccess token={token} tokenId={tokenId}/>)
+
+  }
+  catch(error){
+    const errorJson=error as errorInterface;
+    if(errorJson.errorCode=="BadRequest"){
+      console.log("rofl");
+    }
+    return(<ConfirmationFailure/>)
+  }
+
+}
+
+
+const ConfirmationSuccess = (
+  {token,tokenId}:
+  {token:string|null,tokenId:string|null}
+) =>{
+  return(
+    <>
+      <div className="w-56">
+        <div className="break-all">
+
+        MyPost 
+        <br/> token:{token&&token.split(5)}
+        <br/> tokenID:{tokenId}
+        </div>
+      </div>
+    </>
+  )
+}
+
+const ConfirmationFailure = (
+) =>{
+  return(
+    <>
+      <div className="w-56">
+        <div className="break-all">
+
+        MyPost 
+        Confirmation Failed
+        </div>
+      </div>
+    </>
+  )
+}
+
