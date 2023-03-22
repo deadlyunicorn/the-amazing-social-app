24c99172d0d2beb1008002f75b8476b04cb1e37b
deadly-unicorn <linatic@pm.me>
2023-03-22 04:07:58 +0200

    added some animation

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ const Testing = () => {
   return (
     <>
       <div>
-        <div className="bg-white p-4 rounded-lg w-96 h-48">
+        <div className="bg-white p-4 rounded-lg w-96 h-48 animate-appearance">
             <div className="text-center">Login to my App</div>
             <br/>
             <div
