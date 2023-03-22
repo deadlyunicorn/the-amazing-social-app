d89be1701b1b9ffa10e93ee96f31fd7152474f17
deadly-unicorn <linatic@pm.me>
2023-03-22 04:09:47 +0200

    Initial registration page (beta1?)

--- app/(register)/register/page.tsx
+++ app/(register)/register/page.tsx
@@ -1,19 +1,28 @@
 'use client'
-import {app} from "@/app/components/appObject"
-import { useEffect, useState } from "react";
+
 import "@/app/components/Styles/styles.css"
+
+import {  useEffect, useState } from "react";
+
+import {app} from "@/app/components/appObject"
 import * as Realm from "realm-web"
+
 import UserDetails from "@/app/components/userDetails";
 import LogoutButton from "@/app/components/Login_Logout_Register/logoutButton";
-import RegisterForm from "@/app/components/Login_Logout_Register/email/register_form";
 
+import Form from "@/app/components/Login_Logout_Register/email/login_register_form";
+import ErrorHandler from "@/app/components/Login_Logout_Register/email/error_handling";
 
 
 const UserPage = () =>{
 
   const [pageLoad,setPageLoad]=useState(false);
   const [user,setUser]=useState<Realm.User | null>(null);
+  const [errorCode,setErrorCode]=useState<string|null>(null);
 
+
+  const [email,setEmail]=useState<string>("");
+  const [password,setPassword]=useState<string>("");
 
   useEffect(()=>{
     setUser(app.currentUser);
@@ -22,27 +31,45 @@ const UserPage = () =>{
     },500);
   },[user])
 
+  useEffect(()=>{ //I could either use useRef to keep a static
+    //version of email for each form submit
+    //this will close the prompt => unable the user
+    //to send confirmation to the wrong email.
+    setErrorCode(null);
+  },[email])
+
 
   return(
-    <div className="bg-white p-4 rounded-lg w-96 h-48">
-      <div
-        data-pageload={pageLoad}
-        className="data-[pageload=true]:inline animate-hidden hidden">
 
-          {user?
-          <>
-            <UserDetails user={user}/>
-            <LogoutButton setUser={setUser}/>
-            <span>&nbsp;to Signup for a new account.</span>
-          </>:
-          <>
-            Signup today!
-            <RegisterForm/>
-          </>
-          }
+    <div>
+
+      <div className="bg-white p-4 rounded-lg w-96 h-44 mb-4 duration-700 text-center">
+        <div
+          data-pageload={pageLoad}
+          className="data-[pageload=true]:inline animate-hidden hidden">
+
+            {user?
+            <>
+              <UserDetails user={user}/>
+              <LogoutButton setUser={setUser}/>
+              <span>&nbsp;to Signup for a new account.</span>
+            </>:
+            <>
+              Signup today!
+                <Form 
+                  formType="register"
+                  setErrorCode={setErrorCode} 
+                  email={email} setEmail={setEmail} 
+                  password={password} setPassword={setPassword}/>
+
+            </>
+            }
 
 
+        </div>
       </div>
+      {!user&&errorCode&&<ErrorHandler errorCode={errorCode} setErrorCode={setErrorCode} email={email}/>}
+
     </div>
   )
 }
