ebf0c303e7316ba2157f1c2436753457423f4b4f
deadly-unicorn <linatic@pm.me>
2023-03-22 04:07:36 +0200

    updated loading box..

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,12 @@
+import "@/app/components/Styles/styles.css"
+
+
 export default function Loader(){
   return(
     <>
       <div>
-        <div className="bg-white p-4 rounded-lg w-96 h-48">
-            <div className="text-center">Login to my App</div>
+        <div className="bg-white p-4 rounded-lg w-96 h-48 animate-appearance flex flex-col justify-center">
+            <div className="text-center">Loading...</div>
             <br/>
         </div>
 
