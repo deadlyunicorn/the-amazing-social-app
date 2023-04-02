6b6627ce6f88299cddaaaa49a8901a8cbe0d0ca3
deadly-unicorn <retroalex1008@gmail.com>
2023-04-02 04:36:17 +0300

    added reactContext for user status

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from '@/app/components/Header/Header'
 import NavigationBar from '@/app/components/Navigation/Navigation'
 import '@/app/components/Styles/globals.css'
+import UserContext from './components/ContextComponent/contextComp'
 
 export const metadata = {
   title: 'Social App',
@@ -22,17 +23,20 @@ export default function RootLayout({
       <body className='bg-gradient-to-b from-zinc-700 to-zinc-900 
       w-screen h-full 
       flex flex-col overflow-x-hidden
-      relative dark:text-black
+      relative dark:text-black text-black
       '>
-        <Header/>
+        <UserContext>
+
+          <Header/>
+        
+          <main className="flex justify-center mt-28 min-h-screen w-screen">
+            <div className=" py-5 pb-32">
+              {children}
+            </div>
+          </main>
       
-        <main className="flex justify-center mt-28 min-h-screen w-screen">
-          <div className=" py-5 pb-32">
-            {children}
-          </div>
-        </main>
-      
-        <NavigationBar/>  
+          <NavigationBar/>  
+        </UserContext>
       </body>
     </html>
   )
