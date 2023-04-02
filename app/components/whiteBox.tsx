242e21ab732310c5dbc740ae16518b481a9d4800
deadly-unicorn <retroalex1008@gmail.com>
2023-04-02 05:16:41 +0300

    added margin bot

--- app/components/whiteBox.tsx
+++ app/components/whiteBox.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function WhiteBox ({children}:{children:ReactNode}) {
 
   return(
-    <div className="bg-white p-4 rounded-lg w-96 min-h-[192px] animate-appearance flex flex-col justify-between">
+    <div className="bg-white p-4 rounded-lg w-96 min-h-[192px] animate-appearance flex flex-col justify-between mb-4">
       {children}
     </div>
   )
