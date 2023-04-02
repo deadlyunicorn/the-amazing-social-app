a250eeb6c39bbbcaa4635fa0906d9ec465df91f2
deadly-unicorn <retroalex1008@gmail.com>
2023-04-02 04:38:47 +0300

    Added WhiteBox component and appContext

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,39 +1,19 @@
 'use client'
-import {app} from "@/app/components/appObject"
-import { useEffect, useState } from "react";
 import "@/app/components/Styles/styles.css"
-import * as Realm from "realm-web"
 import UserDetails from "@/app/components/userDetails";
-
-
+import WhiteBox from "@/app/components/whiteBox";
 
 
 
 const UserPage = () =>{
 
-  const [pageLoad,setPageLoad]=useState(false);
-  const [user,setUser]=useState<Realm.User | null>(null);
-
-
-  useEffect(()=>{
-    setUser(app.currentUser);
-    setTimeout(()=>{
-      setPageLoad(true);
-    },500);
-  },[user])
-
 
   return(
-    <div className="bg-white p-4 rounded-lg w-96 h-48">
-      <div
-        data-pageload={pageLoad}
-        className="data-[pageload=true]:inline animate-hidden hidden">
-
-        <UserDetails user={user}/>
+      <WhiteBox>
+        <UserDetails/>
         <div className="flex justify-between">
         </div>
-      </div>
-    </div>
+      </WhiteBox>
   )
 }
 
