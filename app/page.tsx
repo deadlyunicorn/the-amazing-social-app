b02e5a9d540bb2cafeba80949b2c9a7b912d1ff4
deadly-unicorn <retroalex1008@gmail.com>
2023-04-02 04:41:28 +0300

    added WhiteBox and appContext.
    idk why, will be hugely updated soon...

--- app/page.tsx
+++ app/page.tsx
@@ -2,69 +2,48 @@
 
 
 //
-import { useEffect, useState } from "react"
+import { useContext} from "react"
 import "@/app/components/Styles/styles.css"
-import {app} from "@/app/components/appObject"
 
 //Realm
-import * as Realm from "realm-web"
 import UserDetails from "@/app/components/userDetails"
 import LoginButton from "@/app/components/Login_Logout_Register/anonymous/loginButton"
-import LogoutButton from "@/app/components//Login_Logout_Register/logoutButton"
-import ErrorHandler from "./components/Login_Logout_Register/email/error_handling"
+import ErrorHandler from "@/app/components/Login_Logout_Register/email/error_handling"
+
+import WhiteBox from "./components/whiteBox"
+import { appContext } from "@/app/components/ContextComponent/contextComp"
 
 
 
 //
 
 
-const Testing = () => {
-
-  const [pageLoad,setPageLoad]=useState(false);
-
-
-  const [user,setUser]=useState<Realm.User | null>(null);
-  const [errorCode,setErrorCode]=useState<string|null>(null);
+const Homepage = () => {
 
 
 
 
-  useEffect(()=>{
-    setUser(app.currentUser);
-    setTimeout(()=>{
-      setPageLoad(true);
-    },500);
-  },[user])
+  const {errorCode}=useContext(appContext)
 
   
   return (
     <>
-      <div>
-        <div className="bg-white p-4 rounded-lg w-96 h-48 animate-appearance">
-            <div
-              data-pageload={pageLoad}
-              className="data-[pageload=true]:inline animate-hidden hidden">
-  
-              <UserDetails user={user}/>
-              <div className="flex justify-between">
-                <LoginButton user={user} setUser={setUser} setErrorCode={setErrorCode}/>
-                {user&&<LogoutButton setUser={setUser}/>}
-              </div>
-            </div>
-        </div>
-        {errorCode&&
+      <WhiteBox>
+        <UserDetails/>
+        <LoginButton/>
+      </WhiteBox>
+
+      {errorCode&&
         <div className="mt-4">
-
-          <ErrorHandler errorCode={errorCode} setErrorCode={setErrorCode}/>
+          <ErrorHandler/>
         </div>
-        }
+      }
 
-      </div>
     </>
   )
 }
 
-export default Testing
+export default Homepage;
 
 
 
