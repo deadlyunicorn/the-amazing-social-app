52192ef8e52e7d6037e79e52d4becc0dccbda513
deadlyunicorn <retroalex1008@gmail.com>
2023-09-07 03:55:28 +0300

    +semantic HTML

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,21 +2,28 @@ import WhiteBox from "@/app/components/whiteBox"
 
 import About from "@/app/about/about"
 import UserDetails from "@/app/components/userDetails";
+import { LoadingScreen } from "../components/Loading/LoadingAnimation";
 
 
 const Homepage = () => {
 
   return (
     <>
-       <WhiteBox>
-        <About/>
-      </WhiteBox>
+      <main>
+
+        <WhiteBox>
+          <About/>
+        </WhiteBox>
+
+      </main>
 
 
+      <aside>
 
-      <WhiteBox>
-          <UserDetails/>
-      </WhiteBox>
+        <WhiteBox>
+            <UserDetails/>
+        </WhiteBox>
+      </aside>
 
 
     </>
