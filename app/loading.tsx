9e79be0209a7cbf3fe87eafc851d2b7ffaa64412
deadlyunicorn <retroalex1008@gmail.com>
2023-09-10 05:06:44 +0300

    minor css fixes

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,14 +4,15 @@ import { LoadingScreen } from "@/app/(components)/Loading/LoadingAnimation"
 
 export default function Loader(){
   return(
-    <main className="
+    <section className="
+    bg-transparent
     top-[30%]
     flex justify-center
     fixed left-0 w-full">
 
      <LoadingScreen height={200}/>
   
-    </main>
+    </section>
 
   )
 }
