fb801be2f8c79e496ca27a52e3ee040fe36edfd7
deadlyunicorn <retroalex1008@gmail.com>
2023-09-11 04:56:44 +0300

    +Logout form

---
+++ app/(components)/LogoutForm.tsx
@@ -0,0 +1,15 @@
+import { cookies } from "next/headers";
+import { supabaseCredentials } from "../(supabase)/global";
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+
+export const LogOutForm = () => (
+  <form
+        action={
+          async()=>{
+            "use server"
+            const supabase = createServerActionClient({cookies},supabaseCredentials);
+            await supabase.auth.signOut();
+          }}>
+        <button className="text-link">Logout</button>
+  </form>
+)
