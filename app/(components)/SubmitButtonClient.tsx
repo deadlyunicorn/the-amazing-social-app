2248fa26d2895757f489496fdedce9c25d55abd0
deadlyunicorn <retroalex1008@gmail.com>
2023-09-11 05:00:00 +0300

    +EmailPasswordForm (Uses experimental FormStatus)

---
+++ app/(components)/SubmitButtonClient.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { experimental_useFormStatus as useFormStatus } from "react-dom"
+
+export const SubmitButtonClient = () => {
+
+
+  const {pending}=useFormStatus();
+
+
+  return(
+    <button
+    className="border bg-slate-100 hover:bg-slate-200 px-2 py-1 rounded-md">
+          {pending?"Loading...":"Submit"}
+    </button>
+  )
+}
+
+
