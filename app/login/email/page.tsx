d4774be8dc6e5a0f0281326d66a51022ed0b2b4e
deadlyunicorn <retroalex1008@gmail.com>
2023-09-11 05:01:41 +0300

    Refactoring email login into components

--- app/login/email/page.tsx
+++ app/login/email/page.tsx
@@ -1,56 +1,43 @@
 import "@/app/(styles)/styles.css"
 import { emailLogin } from "../login"
+import { SignUpAside } from "../page"
+import { ErrorSection } from "@/app/(components)/ErrorSection"
+import { MultipleRowsWrapper } from "@/app/(components)/FormWrapper"
+import { EmailPasswordForm } from "@/app/(components)/EmailPasswordForm"
 
 const LoginForm = (
-  {searchParams}:{
-  searchParams:{
-    error? : string
-  }
-}) =>{
+  { searchParams }: {
+    searchParams: {
+      error?: string
+    }
+  }) => {
 
 
-  return(
-    <div className="flex flex-col w-full gap-y-8 items-center max-w-md">
-    <section className=" bg-gray-900">
+  return (
 
-      
+    <MultipleRowsWrapper>
 
-      <h3 className="
-      
-        text-white 
-        text-center underline">Login today!</h3>
-      <form 
-        action={emailLogin}
-        className="flex flex-col gap-2 px-3 py-2">
-          
-          <input
-            name="email"
-            className="py-1 px-2"
-            placeholder="Email" type={"email"} minLength={10} required/>
-          
-          <input
-            name="password"
-            className="py-1 px-2"
-            placeholder="Password" type={"password"} minLength={6} required/>
+     
+        <EmailPasswordForm
+          formHeader="Login today!"
+          action={emailLogin}
+        />
 
-          <button 
-            className="border bg-slate-100 hover:bg-slate-200 px-2 py-1 rounded-md">
-            Submit
-          </button>
-
-        </form>
-
-        
+      <section>
+        <SignUpAside />
       </section>
 
-      {searchParams.error && 
-        <section
-          className="text-error-light text-center">
-            {searchParams.error}
-        </section>}
-      </div>
+      {searchParams.error &&
+
+        <ErrorSection>
+          {searchParams.error}
+        </ErrorSection>
+      }
+    </MultipleRowsWrapper>
   )
 }
+
+
 
 export default LoginForm;
 
