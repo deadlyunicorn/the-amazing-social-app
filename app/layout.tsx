1e29f0698ebea6a9a716243133587cb6fff3dec3
deadlyunicorn <retroalex1008@gmail.com>
2023-09-13 05:04:47 +0300

    force dynamic pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,8 @@ export const metadata: Metadata = {
   description: 'Social App project by deadlyunicorn!',
 }
 
+export const dynamic = 'force-dynamic'
+
 export default function RootLayout({
   children,
 }: {
