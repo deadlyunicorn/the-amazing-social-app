b6ac81597dcc106870cb501fd5f61b4be5aa06bb
deadlyunicorn <retroalex1008@gmail.com>
2023-09-19 02:52:33 +0300

    +ResetForm OnFormSubmit

---
+++ app/explore/ResetOnSubmit.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { useEffect } from "react"
+import { experimental_useFormStatus } from "react-dom"
+
+export const ResetOnSubmit = ({formId}:{formId:string}) => {
+
+  const {pending} = experimental_useFormStatus();
+
+  useEffect(()=>{
+    if (!pending){
+      //@ts-ignore
+      document.querySelector(formId).reset();
+    }
+  },[pending])
+
+  //if I put <textarea/> on client it doesn't 
+  //validate on client (minLength)
+
+  return(
+    <>
+   
+    </>
+  )
+}
