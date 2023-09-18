fb04c7f5b1dadc1a412d1c21f8c1671195fd67a9
deadlyunicorn <retroalex1008@gmail.com>
2023-09-19 02:48:53 +0300

    +get the first page of posts on the server +Clear error section +Client Components import +Clear form on submit +Add Image to posts

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,26 +1,42 @@
-import { redirect } from "next/navigation";
-import { getPosts, userPost } from "../(mongodb)/getPosts";
 import { SubmitButtonClient } from "../(components)/SubmitButtonClient";
 import { MultipleRowsWrapper } from "../(components)/FormWrapper";
 import { ErrorSection } from "../(components)/ErrorSection";
 import { handleCreatePost } from "./createPost";
-import { formatDate } from "../(lib)/formatDate";
+import { ImageInputOptional } from "../user/updateImage/ImageInput";
+import { ResetOnSubmit } from "./TextArea";
+import Link from "next/link";
+import { PostSection } from "./posts";
+import { getPosts, userPost } from "../(mongodb)/getPosts";
+import { getPostsPageLimit } from "../(lib)/postLimit";
 
-const ExplorePage = async ({ searchParams }: { searchParams: { page: number, error?: string } }) => {
+const ExplorePage = async ({ searchParams }: { searchParams: { error?: string } }) => {
 
-  const page = searchParams.page;
+  const firstPagePosts = (await getPosts({ page: 1 }))?.map(
+    post => (
+      {
+        comments: post.comments,
+        likers: post.likers,
+        content: post.content,
+        created_by: post.created_by,
+        created_at: post.created_at.getTime()
+      }
 
-  if (!(+page) || (+page < 1)) {
-    redirect(`/explore?page=1`);
-  }
+    )
+  );
 
-  const posts = await getPosts({page:1});
+  const maxPages = await getPostsPageLimit()||0;
 
   return (
     <MultipleRowsWrapper>
 
       {searchParams.error &&
         <ErrorSection>
+          <Link
+            href="/explore"
+            className="
+            text-error-light-reactive
+            font-semibold
+            absolute top-2 right-5">X</Link>
           {searchParams.error}
         </ErrorSection>
       }
@@ -31,14 +47,15 @@ const ExplorePage = async ({ searchParams }: { searchParams: { page: number, err
 
 
       </section>
+      
+      {[0].map(()=>
+        <PostSection 
+          maxPages={maxPages}
 
-      <section>
-
-        {posts
-          ? <PostsMap posts={posts} />
-          : <p className="text-center">It seems no one has posted anything yet.</p>
-        }
-      </section>
+        // @ts-ignore (we can't pass Date to client.)
+          firstPagePosts={firstPagePosts} />
+        )
+      }
 
 
 
@@ -47,30 +64,11 @@ const ExplorePage = async ({ searchParams }: { searchParams: { page: number, err
   )
 }
 
-const PostsMap = ({ posts }: { posts: userPost[] }) => {
 
-  return (
-
-    <ul>
-
-      {
-        posts.map(
-          (post, key) => (
-            <li
-              className="border border-dashed text-center" 
-              key={key}>
-              {post.content?.textContent}
-              <br/>
-              By: {post.created_by}@{formatDate(new Date(post.created_at))}
-            </li>
-          )
-        )
-      }
-    </ul>
-  )
-}
 
 const CreatePost = () => {
+
+  const formId = 'postForm';
 
   return (
     <section>
@@ -78,11 +76,20 @@ const CreatePost = () => {
       {/* add images as well !? */}
 
       <form
+        id={formId}
         action={handleCreatePost}
         className="
         my-2
         flex flex-col 
         items-center gap-y-4">
+
+        <label
+          className="text-link hover:cursor-pointer"
+          htmlFor="image">Add image</label>
+        <ImageInputOptional pixels={200} />
+
+
+        <ResetOnSubmit formId={formId} />
 
         <textarea
           required
@@ -94,6 +101,8 @@ const CreatePost = () => {
           bg-white bg-opacity-30
           w-full h-36 resize-none"
           name="post" />
+
+
 
         <SubmitButtonClient />
       </form>
