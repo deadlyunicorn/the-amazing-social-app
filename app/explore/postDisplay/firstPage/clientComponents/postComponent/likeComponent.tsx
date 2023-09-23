55767fd4e0d83b8a795e0cca6d012592011e7de0
deadlyunicorn <retroalex1008@gmail.com>
2023-09-23 07:08:18 +0300

    refactoring + new route folder + minor adjustments

--- app/explore/postDisplay/firstPage/clientComponents/postComponent/likeComponent.tsx
+++ app/explore/postDisplay/firstPage/clientComponents/postComponent/likeComponent.tsx
@@ -3,10 +3,14 @@
 import { userDetailsClient } from "../../../../page"
 import { useEffect, useState } from "react";
 import { UpdateResult } from "mongodb";
+import Link from "next/link";
+import { userPostWithAvatar } from "../../../(mongodb)/getPosts";
 
 
-export const LikeComponent = ({ postId, initialLikers, userDetails }: { postId: string, initialLikers: string[], userDetails: userDetailsClient | null }) => {
+export const LikeComponent = ({ post, userDetails }: { post:userPostWithAvatar, userDetails: userDetailsClient | null }) => {
 
+  const initialLikers = post.likers.map(liker => String(liker));
+  const postId = post._id;
   const viewerName = String(userDetails?.username);
 
   const [hasMounted, setHasMounted] = useState(false);
@@ -32,8 +36,8 @@ export const LikeComponent = ({ postId, initialLikers, userDetails }: { postId: 
         (async () => {
 
           likers.includes(viewerName)
-            ? await fetch(`/explore/post/${postId}/`, { method: "POST", body: JSON.stringify({ like: true }) })
-            : await fetch(`/explore/post/${postId}/`, { method: "POST", body: JSON.stringify({ like: false }) })
+            ? await fetch(`/explore/post/${postId}/like`, { method: "POST", body: JSON.stringify({ like: true }) })
+            : await fetch(`/explore/post/${postId}/like`, { method: "POST", body: JSON.stringify({ like: false }) })
 
               .then(async (response) => {
                 try {
@@ -64,6 +68,22 @@ export const LikeComponent = ({ postId, initialLikers, userDetails }: { postId: 
 
   }, [likers])
 
+  const likersCount = likers.length;
+  const latestLikers = likers
+    .slice( 0, Math.min( 3, likersCount ) )
+    .reverse()
+    .map( (liker) => 
+      <Link key={liker} href={`/user/${liker}`}>
+        <span className="text-sm"> 
+          { liker } 
+        </span>
+      </Link>
+    )
+
+  const otherLikers = likersCount > 4
+    ? `and ${likersCount} others` 
+    : likersCount == 4 && `and 1 other`; 
+
 
   return (
       <div className="flex items-center gap-x-2">
@@ -78,7 +98,9 @@ export const LikeComponent = ({ postId, initialLikers, userDetails }: { postId: 
         }}>
         <svg
           data-pending={likers.includes(viewerName)}
-          className="cursor-pointer stroke-red-600 data-[pending=true]:fill-red-600 fill-transparent"
+          className="cursor-pointer 
+          stroke-red-600 data-[pending=true]:fill-red-600 
+          fill-transparent"
           xmlns="http://www.w3.org/2000/svg"
           width={30}
           version="1.1"
@@ -90,8 +112,7 @@ export const LikeComponent = ({ postId, initialLikers, userDetails }: { postId: 
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeOpacity="1"
-              strokeWidth="0.735"
-              d="M20.01 19.145c-.4.005-.874.044-1.216.282-1.063.74-1.806 1.994-2.134 3.247-.362 1.379-.262 2.953.292 4.267.867 2.058 2.78 3.528 4.463 4.998 1.048.917 3.46 2.341 3.46 2.341a.245.245 0 00.057.014.187.187 0 00.068.017.188.188 0 00.069-.017.247.247 0 00.056-.014s2.412-1.424 3.46-2.34c1.682-1.47 3.596-2.94 4.463-5 .553-1.313.654-2.887.292-4.266-.328-1.253-1.072-2.507-2.134-3.247-.342-.238-.816-.277-1.217-.282-1.203-.014-2.35.699-3.231 1.492-.85.764-1.447 2.513-1.758 2.944-.311-.43-.908-2.18-1.758-2.944-.882-.793-2.029-1.506-3.231-1.492z"
+              strokeWidth="0.735" d="M20.01 19.145c-.4.005-.874.044-1.216.282-1.063.74-1.806 1.994-2.134 3.247-.362 1.379-.262 2.953.292 4.267.867 2.058 2.78 3.528 4.463 4.998 1.048.917 3.46 2.341 3.46 2.341a.245.245 0 00.057.014.187.187 0 00.068.017.188.188 0 00.069-.017.247.247 0 00.056-.014s2.412-1.424 3.46-2.34c1.682-1.47 3.596-2.94 4.463-5 .553-1.313.654-2.887.292-4.266-.328-1.253-1.072-2.507-2.134-3.247-.342-.238-.816-.277-1.217-.282-1.203-.014-2.35.699-3.231 1.492-.85.764-1.447 2.513-1.758 2.944-.311-.43-.908-2.18-1.758-2.944-.882-.793-2.029-1.506-3.231-1.492z"
               display="inline"
               paintOrder="stroke markers fill"
             ></path>
@@ -99,7 +120,8 @@ export const LikeComponent = ({ postId, initialLikers, userDetails }: { postId: 
         </svg>
       </button>
 
-        <p>{likers.length}</p>
+        <p className="flex gap-x-1">
+          {likersCount} {latestLikers} {otherLikers}  </p>
       </div>
   )
 }
