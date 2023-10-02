2f3b0500b09fe9cbe21d5f24b6daaa6f4c6f8d10
deadlyunicorn <retroalex1008@gmail.com>
2023-10-02 23:04:11 +0300

    removed unecessary stuff

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,60 +1,26 @@
 import "@/app/(styles)/styles.css"
 import { ErrorSection } from "@/app/(components)/ErrorSection"
 import { MultipleRowsWrapper } from "@/app/(components)/FormWrapper"
-import { EmailPasswordForm } from "@/app/(components)/EmailPasswordForm"
-import Link from "next/link"
-import { emailLogin } from "./login"
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
-import { cookies } from "next/headers"
-import { LogOutForm } from "../settings/LogoutForm"
+import { Suspense } from "react"
+import { LoginPageSynced } from "./AsyncDetails"
+import { LoadingScreen } from "../(components)/Loading/LoadingAnimation"
 
-const LoginPage = async(
+const LoginPage = (
   { searchParams }: {
     searchParams: {
       error?: string
     }
   }) => {
 
-    const supabase = createServerComponentClient(
-      {cookies},
-      {supabaseKey:process.env.supabaseKey,
-      supabaseUrl:process.env.supabaseUrl}
-    )
-    
-    const session = (await supabase.auth.getSession()).data.session;
-  
 
 
   return (
     <MultipleRowsWrapper>
 
-      {session
-        ?
-        <section> 
-          <center>
-            <h2>Hello there!</h2>
-            <p>You are logged in with {session.user.email}.</p>
-            <LogOutForm/>
-          </center>
-        </section>
-        
-        : 
-        <>
-          <EmailPasswordForm
-            formHeader="Login today!"
-            action={emailLogin}
-          />
-
-          <section className="text-center">
-              <Link 
-                tabIndex={0} href="/account/recover">
-                I forgot my password :&#40;
-              </Link>
-            <SignUpAside />
-          </section>
-        </>
-        
-        }
+      <Suspense fallback={<LoadingScreen height={200} />}>
+        <LoginPageSynced/>
+      </Suspense>
+      
         {
           searchParams.error &&
 
@@ -75,19 +41,7 @@ export default LoginPage;
 
 
 
-const SignUpAside = () => {
 
-    return (
-      <aside className="flex flex-col items-center mt-4">
-            <p>
-              Don&apos;t have an account?
-            </p>
-            <Link tabIndex={0} href="/register">
-                Sign up
-            </Link>
-      </aside>
-    )
-}
 
 
 
--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,12 @@
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 
-import type { NextRequest } from 'next/server'
-import type { Database } from '@/database.types'
+import { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
   
-  const res = NextResponse.next()
-  const supabase = createMiddlewareClient<Database>(
-    { req, res },{supabaseKey:process.env.supabaseKey,supabaseUrl:process.env.supabaseUrl }
-  );
-  await supabase.auth.getSession();
-  return res
+  if( req.nextUrl.pathname == "/" ){
+    return NextResponse.redirect(`${process.env.SERVER_URL}/about`);
+  } 
+
 }
