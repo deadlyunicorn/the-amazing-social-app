e15d6307945be48bf51d7d2a0d2023eeddabac03
deadlyunicorn <retroalex1008@gmail.com>
2023-10-04 02:38:21 +0300

    +added some comments

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,11 @@ import { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
   
+  //Account settings page: if user isn't logged in redirect them to /login (if they access any of the /account/[subdomains])
+  //Implement it after you add Auth.js support.
+
   if( req.nextUrl.pathname == "/" ){
     return NextResponse.redirect(`${process.env.SERVER_URL}/about`);
-  } 
+  }
 
 }
