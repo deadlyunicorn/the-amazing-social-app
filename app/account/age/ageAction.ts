e81b9756274308fa449d10b35c022f294cfc5aba
deadlyunicorn <retroalex1008@gmail.com>
2023-10-04 17:47:45 +0300

    +change age page

---
+++ app/account/age/ageAction.ts
@@ -0,0 +1,57 @@
+"use server"
+
+
+import { getMongoClient } from "@/app/(lib)/mongoClient";
+import { getSessionDetails, userObject } from "@/app/(mongodb)/user";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import * as zod from "zod"
+
+export const changeAge = async( formData: FormData )=>{
+
+  const currentYear = new Date().getFullYear();
+
+  const newAge = await zod
+    .number()
+    .min( currentYear - 110 )
+    .max( currentYear -  18 )
+    .parseAsync( +String( formData.get('age') ) )
+    .catch( err=>{
+      console.log(err)
+      redirect(`/account/age?error=${"Invalid year"}`)
+    });
+
+
+  const client = getMongoClient();
+  const session = await getSessionDetails();
+
+  if ( session?.age == newAge){
+    redirect(`/account/age?error=${"This is already your age"}`);
+  }
+
+  if ( session?.ageChanged ){
+    redirect(`/account/age?error=${"You can only update your age once. Contact support if there's an issue."}`);
+  }
+  try{
+
+    const users = client.db('the-amazing-social-app').collection('users');
+
+    await users.findOneAndUpdate(
+      {_id:session?._id},
+      { $set:{
+        age: newAge,
+        ageChanged: true
+      }}
+    );
+    revalidatePath('/');
+  }
+  catch(error){
+    if (error === "age used"){
+      redirect(`/account/age?error=${"age is already taken"}`);
+    }
+    redirect(`/account/age?error=${"Failed Updating"}`);
+  }
+  finally{
+    client.close();
+  }
+}
--- app/account/age/page.tsx
+++ app/account/age/page.tsx
@@ -1,24 +1,60 @@
-import { getSessionDetails } from "@/app/(mongodb)/user";
+import { ErrorSection } from "@/app/(components)/ErrorSection";
+import { MultipleRowsWrapper } from "@/app/(components)/FormWrapper";
+import { getSessionDetails, userObject } from "@/app/(mongodb)/user";
 import { redirect } from "next/navigation";
+import { SubmitButtonClient } from "@/app/(components)/SubmitButtonClient";
+import { changeAge } from "./ageAction";
 
-const ChangeAgePage = async() => {
+const ChangeAgePage = async({searchParams}:{searchParams:{error:string}}) => {
 
   const sessionDetails = await getSessionDetails();
   if ( !sessionDetails ){
     redirect('/user');
   }
+  
+  const currentYear = new Date().getFullYear();
 
-  return(
+  return (
+  <MultipleRowsWrapper>
+  <section>
+    <h3 className="text-center">Your current birth year is set to: 
+      <br/>{sessionDetails.age}
+    </h3>
+    
+    <form
+      className="flex flex-col items-center gap-y-2 my-2"
+      action={changeAge}
+      >
+      
+      <label
+        htmlFor="age">
+          Enter the new birth year:
+      </label>
+      <input
+        required
+        id="age"
+        placeholder="birth year" 
+        aria-label="insert your correct birthyear"
+        name="age"
+        type="number"
+        min={currentYear-110}
+        max={currentYear-18}
+        />
+        
+      <SubmitButtonClient/>
+    </form>
+    
+  </section>
 
-    <section>
-      <h3 className="text-center">You currently were born in:
-        <br/>{sessionDetails.age}
-      </h3>
-      Change your age
-    </section>
+  {searchParams.error &&
+  <ErrorSection path="/account/age">
+    {searchParams.error}
+  </ErrorSection>
+  }
+  </MultipleRowsWrapper>
   )
 
-
 }
+
 
 export default ChangeAgePage;
