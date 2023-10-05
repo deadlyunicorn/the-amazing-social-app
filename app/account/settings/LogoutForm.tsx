67b65d0e7b31e14a5f426f203e38642644c8335b
deadlyunicorn <retroalex1008@gmail.com>
2023-10-06 01:37:37 +0300

    +new log out form logic (not yet verified to work)

--- app/account/settings/LogoutForm.tsx
+++ app/account/settings/LogoutForm.tsx
@@ -1,14 +1,44 @@
-import { cookies } from "next/headers";
-import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
-import { supabaseCredentials } from "@/app/(supabase)/global";
+import { revalidatePath } from "next/cache";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export const LogOutForm = async() => (
   <form
         action={
           async()=>{
             "use server"
-            const supabase = createServerActionClient({cookies},supabaseCredentials);
-            await supabase.auth.signOut();
+            const headerList = headers();
+            const cookieHeader = String( headerList.get('cookie') );
+            const authHeader =   String( headerList.get('authorization') );
+
+
+            const csrfToken = await fetch(`${process.env.SERVER_URL}/api/auth/csrf`,{
+              headers:[
+                ["cookie",cookieHeader],
+                ["authorization", authHeader]
+              ],
+            })
+              .then( async( res ) => await res.json() )
+              .then( csrfTokenObject => csrfTokenObject?.csrfToken );
+
+            await fetch(`${process.env.SERVER_URL}/api/auth/signout`,{
+              method:"POST",
+              headers:[
+                ["cookie",cookieHeader],
+                ["authorization", authHeader]
+              ],
+              body: JSON.stringify({
+                csrfToken: csrfToken
+              })
+            })
+            .then( async(res)=> {
+              if ( res.ok ) {
+                // const result = await res.json();
+                // console.log(result)
+                revalidatePath('/');
+                redirect('/explore')
+              }
+            })
           }}>
         <button className="text-link">Logout</button>
   </form>
