2200de3aed69aa7b866a106fa3e0cb329a9ef89b
deadlyunicorn <retroalex1008@gmail.com>
2023-10-06 01:37:13 +0300

    return session with "id" property. +1 mongoclient per server +type declaration

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,34 @@
 // import { customUser } from "@/app/api/verification"
-import NextAuth from "next-auth"
+import NextAuth, { Session, User } from "next-auth"
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import { getMongoClient } from "@/app/lib/mongoClient"
 import { credentialsProvider, emailProvider, githubProvider, googleProvider } from "./providers";
+import { ObjectId } from "mongodb";
+import { mongoClient } from "../../mongodb/client";
+
+ //pass this like that so that it doesn't create a new client
+//for every auth request.
 
 export const authOptions = {
-  adapter: MongoDBAdapter( getMongoClient().connect(), {
+  adapter: MongoDBAdapter( mongoClient.connect(), {
     databaseName: "the-amazing-social-app-auth",
   } ),
+  callbacks:{
+    async session({session, token, user }:{ session: Session|undefined, token: string|undefined, user:User|undefined}){
+      
+      if ( session && session.user && user){
+
+        return { 
+          user :{ 
+            ...session.user,
+            id: new ObjectId(user.id)
+          },
+          expires: session.expires
+        }
+      }
+
+      return session;
+    }
+  },
   providers: [ googleProvider, githubProvider, emailProvider, credentialsProvider],
   // pages: {
   //    signIn: '/admin/login' //default is /api/auth/signin
