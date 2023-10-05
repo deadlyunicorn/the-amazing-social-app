71590b61a25299ecbd5c50e2dcdfea3fe8c8cbb1
deadlyunicorn <retroalex1008@gmail.com>
2023-10-05 22:25:54 +0300

    better /user redirection handling (via middleware)

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
+import { getAuthSession, getUserDetails } from './app/api/mongodb/user';
+import { redirect } from 'next/navigation';
 
 export async function middleware(req: NextRequest) {
   
@@ -8,6 +10,39 @@ export async function middleware(req: NextRequest) {
 
   if( req.nextUrl.pathname == "/" ){
     return NextResponse.redirect(`${process.env.SERVER_URL}/about`);
+  }
+  else if ( req.nextUrl.pathname == "/user/" ){
+
+    //@ts-ignore
+    const authSession = ( await getAuthSession() );
+
+    if ( authSession ){
+
+      const user = await getUserDetails();
+
+      const error = req.nextUrl.searchParams.get("error");
+      if ( user ){
+
+        const username = user.username
+        if ( error ){
+          redirect(`/user/${ username }?error=${error}`);
+        }
+  
+        redirect(`/user/${ username }`);
+      }
+      else{
+
+        const email = authSession.email;
+        // TODO const username = authSession.username; to be implemented
+        if (error){
+          redirect(`/user/${ email /*|| username*/ }?error=${error}`);
+        }
+        redirect(`/user/${ email /*|| username*/ }`);
+      }
+    }
+    else{
+      redirect(`/login`);
+    }
   }
 
 }
