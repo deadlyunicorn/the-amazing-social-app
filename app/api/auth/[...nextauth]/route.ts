e158c9692b13764781d549af5c13a05c7a18d245
deadlyunicorn <retroalex1008@gmail.com>
2023-10-06 20:47:37 +0300

    temp disable database for authentication ( I couldn't log in with credentials when it was on ) => temp disable emailProvider +email sent page

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,24 +3,27 @@ import NextAuth, { Session, User } from "next-auth"
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import { credentialsProvider, emailProvider, githubProvider, googleProvider } from "./providers";
 import { ObjectId } from "mongodb";
-import { mongoClientPromise } from "../../mongodb/client";
+import { mongoClient } from "../../mongodb/client";
 
  //pass this like that so that it doesn't create a new client
 //for every auth request.
 
 export const authOptions = {
-  adapter: MongoDBAdapter( mongoClientPromise, {
-    databaseName: "the-amazing-social-app-auth",
-  } ),
+  //@ts-ignore
+  // adapter: MongoDBAdapter( mongoClient, {
+    // databaseName: "the-amazing-social-app-auth",
+  // } ),
   callbacks:{
     async session({session, token, user }:{ session: Session|undefined, token: string|undefined, user:User|undefined}){
       
+      const id = user?.id;
+
       if ( session && session.user && user){
 
         return { 
           user :{ 
             ...session.user,
-            id: new ObjectId(user.id)
+            id: new ObjectId(id)
           },
           expires: session.expires
         }
@@ -29,11 +32,13 @@ export const authOptions = {
       return session;
     }
   },
-  providers: [ googleProvider, githubProvider, emailProvider, credentialsProvider],
+  providers: [ googleProvider, githubProvider, credentialsProvider,  //emailProvider,
+  ],
   pages: {
      signIn: '/login',
      signOut: '/account/settings',
-     error:  '/login'
+     error:  '/login',
+     verifyRequest: "/success"
   },
 }
 
---
+++ app/success/page.tsx
@@ -0,0 +1,14 @@
+const RegistrationComplete = () => {
+
+  return (
+    <section className="flex flex-col justify-between">
+      <h1>Success!</h1>
+      <p className="text-center pt-2"> Your request was successful.
+        <br/>Check your email for further actions.
+        <br/>
+      </p>
+    </section>
+  )
+}
+
+export default RegistrationComplete;
