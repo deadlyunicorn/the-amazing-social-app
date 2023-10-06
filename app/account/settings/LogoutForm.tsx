67104e2438f57e9314ca0dda1f523df40762d3a2
deadlyunicorn <retroalex1008@gmail.com>
2023-10-06 04:09:03 +0300

    +new logout form

--- app/account/settings/LogoutForm.tsx
+++ app/account/settings/LogoutForm.tsx
@@ -1,45 +1,26 @@
-import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
-import { redirect } from "next/navigation";
 
-export const LogOutForm = async() => (
+export const LogOutForm = async() => {
+
+  const headerList = headers();
+  const cookieHeader = String( headerList.get('cookie') );
+  const authHeader =   String( headerList.get('authorization') );
+
+  const csrfToken = await fetch(`${process.env.SERVER_URL}/api/auth/csrf`,{
+    headers:[
+      ["cookie",cookieHeader],
+      ["authorization", authHeader]
+    ],
+  })
+  .then( async( res ) => await res.json() )
+  .then( csrfTokenObject => csrfTokenObject?.csrfToken );
+    
+  return (
   <form
-        action={
-          async()=>{
-            "use server"
-            const headerList = headers();
-            const cookieHeader = String( headerList.get('cookie') );
-            const authHeader =   String( headerList.get('authorization') );
-
-
-            const csrfToken = await fetch(`${process.env.SERVER_URL}/api/auth/csrf`,{
-              headers:[
-                ["cookie",cookieHeader],
-                ["authorization", authHeader]
-              ],
-            })
-              .then( async( res ) => await res.json() )
-              .then( csrfTokenObject => csrfTokenObject?.csrfToken );
-
-            await fetch(`${process.env.SERVER_URL}/api/auth/signout`,{
-              method:"POST",
-              headers:[
-                ["cookie",cookieHeader],
-                ["authorization", authHeader]
-              ],
-              body: JSON.stringify({
-                csrfToken: csrfToken
-              })
-            })
-            .then( async(res)=> {
-              if ( res.ok ) {
-                // const result = await res.json();
-                // console.log(result)
-                revalidatePath('/');
-                redirect('/explore')
-              }
-            })
-          }}>
+        action={`${process.env.SERVER_URL}/api/auth/signout`}
+        method="POST">
+        <input name="csrfToken" readOnly value={csrfToken} hidden/>
         <button className="text-link">Logout</button>
   </form>
-)
+  )
+}
