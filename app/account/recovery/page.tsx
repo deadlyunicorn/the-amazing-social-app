3d671a66809fed7d03a2540daf1c602379bb1743
deadlyunicorn <retroalex1008@gmail.com>
2023-10-06 20:45:17 +0300

    +Account recovery page with action

--- app/account/recovery/page.tsx
+++ app/account/recovery/page.tsx
@@ -2,6 +2,7 @@ import { ErrorSection } from "@/app/lib/components/ErrorSection";
 import { MultipleRowsWrapper } from "@/app/lib/components/FormWrapper";
 import { SubmitButtonClient } from "@/app/lib/components/SubmitButtonClient";
 import { MessageTextarea } from "./MessageTextarea";
+import { RecoveryAction } from "./recoveryAction";
 
 const AccountRestore = ({searchParams}:{searchParams:{error:string}}) =>{
 
@@ -24,12 +25,12 @@ const AccountRestore = ({searchParams}:{searchParams:{error:string}}) =>{
             placeholder="your@email.com"
             className="bg-slate-200" 
             name="contact" minLength={6} type="email"/>
-          {/* <SubmitButtonClient/> */}
+          <SubmitButtonClient/>
           
         </form>
       </section>
       {searchParams.error &&
-        <ErrorSection path="/account/recover">
+        <ErrorSection path="/account/recovery">
           {searchParams.error}
         </ErrorSection>
       }
@@ -40,15 +41,3 @@ const AccountRestore = ({searchParams}:{searchParams:{error:string}}) =>{
 }
 
 export default AccountRestore;
-const RecoveryAction = async( formData: FormData)=> {
-  "use server"
-
-  return await fetch(`${process.env.SERVER_URL}/api/auth/credentials/recovery`,{
-    method:"POST",
-    body: JSON.stringify({
-      message:formData.get('message'),
-      contact:formData.get('contact')
-    })
-  })
- 
-}
