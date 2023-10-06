8be0c717258fca9ad3757f78692eacde1e01cea5
deadlyunicorn <retroalex1008@gmail.com>
2023-10-06 05:56:01 +0300

    updated pool size

--- app/api/mongodb/client.ts
+++ app/api/mongodb/client.ts
@@ -1,5 +1,7 @@
 import { MongoClient } from "mongodb";
 
-export const mongoClient = new MongoClient(process.env.MONGODB_URI!);
+export const mongoClient = new MongoClient(process.env.MONGODB_URI!,{
+  maxPoolSize: 10,
+});
 
 export const mongoClientPromise = mongoClient.connect();
