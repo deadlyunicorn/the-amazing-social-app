34e848f962fe16f9e22b3be9c8004f5feaeda7f6
deadlyunicorn <retroalex1008@gmail.com>
2023-10-06 21:59:13 +0300

    css fix

--- app/login/setCsrfCookie.tsx
+++ app/login/setCsrfCookie.tsx
@@ -6,10 +6,12 @@ import Link from "next/link";
 export const SetCsrfToken = () => {
 
   return (
-    <div className="bg-black bg-opacity-20 
-    backdrop-blur-sm
-    h-screen w-screen fixed top-0 left-0 z-30">
-    <section className="flex flex-col justify-between animate-none absolute bottom-[10%]">
+    <div className="
+      flex flex-col items-center
+      bg-black bg-opacity-20 
+      backdrop-blur-sm
+      h-screen w-screen fixed top-0 left-0 z-30">
+    <section className="flex flex-col justify-between animate-none absolute bottom-[40%] max-w-md">
       <h3 className="text-center">
         In order for Authentication to work 
         <br/>We need to set an Authorization Cookie.
