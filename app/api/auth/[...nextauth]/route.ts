aab2ffac3d67ccb9c6649910d06dc744eafea18e
deadlyunicorn <petrache.dev@gmail.com>
2024-02-12 12:20:02 +0200

    fixed some export error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,65 +1,10 @@
 // import { customUser } from "@/app/api/verification"
 import NextAuth, { Session, User } from "next-auth"
-import { credentialsProvider, githubProvider, googleProvider } from "./providers";
-import { mongoClient } from "../../mongodb/client";
-import { ObjectId } from "mongodb";
+import { authOptions } from "./authOptions"
 
  //pass this like that so that it doesn't create a new client
 //for every auth request.
 
-export const authOptions = {
-  //@ts-ignore
-  // adapter: MongoDBAdapter( mongoClient, {
-    // databaseName: "the-amazing-social-app-auth",
-  // } ),
-  callbacks:{
-    async session({session, token, user }:{ session: Session|undefined, token: string|undefined, user:User|undefined}){
-      
-      const id =  session?.user?.name?.split('^')[1] || session?.user?.name;
-
-      if ( session && session.user && id ){
-
-        const accounts = mongoClient.db('the-amazing-social-app-auth').collection('accounts');
-        return await accounts.findOne({_id: new ObjectId( id )})
-        .then ( res => {
-          if ( res ){
-            return { 
-              user :{ 
-                ...session.user,
-                id: id
-              },
-              expires: session.expires
-            }
-          }
-          else return null;
-        });
-        //the above gives us back our sanity after trying to log out the user
-        //after deleting their accounts ( willingly )
-        
-        //it is also a double win, as it automatically logs out the user
-        //if e.g. we delete them for their actions.
-
-        //the only drawback is performance. 
-        //(get the data), hopefully that's fast as we keep the connection open.
-
-
-        
-      }
-      else {
-        return null;
-      }
-
-    },
-  },
-  providers: [ googleProvider, githubProvider, credentialsProvider,  //emailProvider, 
-],
-  pages: {
-     signIn: '/login',
-     signOut: '/account/settings',
-     error:  '/login',
-     verifyRequest: "/success"
-  },
-}
 
 //@ts-ignore
 const handler = NextAuth( authOptions );
